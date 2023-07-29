a4b92a5181c450034fdf71fec9565ace407032c4
Joaquin Fischer <joaquin.fischer@gmail.com>
2023-07-29 22:58:07 +0200

    fix: cloudinary vercel cors headers

--- next.config.js
+++ next.config.js
@@ -49,22 +49,22 @@ const moduleExports = {
       {
         source: "/:path*",
         headers: [
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block"
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN"
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff"
-          },
-          {
-            key: "Strict-Transport-Security",
-            value: "max-age=63072000; includeSubDomains; preload"
-          },
+          // {
+          //   key: "X-XSS-Protection",
+          //   value: "1; mode=block"
+          // },
+          // {
+          //   key: "X-Frame-Options",
+          //   value: "SAMEORIGIN"
+          // },
+          // {
+          //   key: "X-Content-Type-Options",
+          //   value: "nosniff"
+          // },
+          // {
+          //   key: "Strict-Transport-Security",
+          //   value: "max-age=63072000; includeSubDomains; preload"
+          // },
           {
             key: "Content-Security-Policy",
             value: "default-src 'self' coe-web-cms.fly.dev; img-src 'self' res.cloudinary.com data:;"
