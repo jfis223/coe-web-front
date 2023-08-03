d8bdc08cbbb70e2523ee785ec29c6d43e9780c3c
Joaquin Fischer <joaquin.fischer@gmail.com>
2023-08-03 13:58:22 +0200

    feat: added lightbox to image gallery block

--- src/ui/features/blocks/views/gallery_block/components/gallery_block.tsx
+++ src/ui/features/blocks/views/gallery_block/components/gallery_block.tsx
@@ -7,6 +7,12 @@ import { breakpoints } from "@/src/ui/styles/breakpoints";
 import { useEffect } from "react";
 import { useState } from "react";
 import type { Photo } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
 
 const NextJsImage = ({ photo, imageProps: { alt, title, sizes, className, onClick }, wrapperStyle }: RenderPhotoProps) => {
   return (
@@ -22,6 +28,7 @@ const NextJsImage = ({ photo, imageProps: { alt, title, sizes, className, onClic
   );
 };
 export const GalleryBlock = ({ images }: CMSGallery) => {
+  const [index, setIndex] = useState(-1);
   const [parsedImages, setParsedImages] = useState<Photo[] | null>(null);
   useEffect(() => {
     const newImages = images.map((image) => {
@@ -43,6 +50,14 @@ export const GalleryBlock = ({ images }: CMSGallery) => {
         renderPhoto={NextJsImage}
         defaultContainerWidth={breakpoints.lm}
         sizes={{ size: "calc(100vw - 240px)" }}
+        onClick={({ index }) => setIndex(index)}
+      />
+      <Lightbox
+        slides={parsedImages ?? []}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        plugins={[Fullscreen, Slideshow, Thumbnails]}
       />
     </Styled.Wrapper>
   );
