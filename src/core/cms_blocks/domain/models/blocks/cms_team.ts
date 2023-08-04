6baba90510b1d0c9ec826369a42ec1a43ea3aff0
Joaquin Fischer <joaquin.fischer@gmail.com>
2023-08-04 14:45:48 +0200

    feat: added team block field to class

--- src/core/cms_blocks/domain/models/blocks/cms_team.ts
+++ src/core/cms_blocks/domain/models/blocks/cms_team.ts
@@ -1,22 +1,24 @@
-import type {ConstructorType} from "@/src/common/interfaces/constructor_type";
-import type {CMSTeamMember} from "@/src/core/cms_blocks/domain/models/items/cms_team_member";
+import type { ConstructorType } from "@/src/common/interfaces/constructor_type";
+import type { CMSTeamMember } from "@/src/core/cms_blocks/domain/models/items/cms_team_member";
 
 export class CMSTeam {
-    id: number;
-    __component: "blocks.team";
-    teamMembers: CMSTeamMemberData;
+  id: number;
+  __component: "blocks.team";
+  teamMembers: CMSTeamMemberData;
+  showBiography: boolean;
 
-    constructor(input: ConstructorType<CMSTeam>) {
-        this.id = input.id;
-        this.__component = input.__component;
-        this.teamMembers = input.teamMembers;
-    }
+  constructor(input: ConstructorType<CMSTeam>) {
+    this.id = input.id;
+    this.__component = input.__component;
+    this.teamMembers = input.teamMembers;
+    this.showBiography = input.showBiography;
+  }
 }
 
 export class CMSTeamMemberData {
-    data: CMSTeamMember[];
+  data: CMSTeamMember[];
 
-    constructor(input: ConstructorType<CMSTeamMemberData>) {
-        this.data = input.data;
-    }
+  constructor(input: ConstructorType<CMSTeamMemberData>) {
+    this.data = input.data;
+  }
 }
